cc840b29513b308e71454dbd86856deeb461abfe
NguyenVanHoang19 <nguyenvanhoang34iuh@gmail.com>
2020-11-06 17:27:31 +0700

    update search

--- index.js
+++ index.js
@@ -61,3 +61,84 @@ app.post("/api/addLinhKien",(req,res)=>{
            // return res.json({msg:"Thêm thành công!!!!"});
     });
 });
+// render form update
+app.post("/updateForm",(req,res)=>{
+    const {maLinhKien,ten,donViTinh,gia,thongSoKyThuat} = req.body;
+    console.log(maLinhKien)
+    const linhKien = {
+        maLinhKien : maLinhKien,
+        ten : ten,
+        donViTinh : donViTinh,
+        gia : gia,
+        thongSoKyThuat : thongSoKyThuat
+    };
+    res.render("formUpdate",{
+        linhKien : linhKien
+    });
+});
+
+// update Linh Kien
+app.post("/updateLinhKien",(req,res)=>{
+    const {maLinhKien,ten,donViTinh,gia,thongSoKyThuat} = req.body;
+    const paramsUpdateLinhKien = {
+        TableName : "LinhKien",
+        Key : {
+            "maLinhKien" : maLinhKien
+        },
+        UpdateExpression: "set ten = :ten , donViTinh = :dvt , gia = :gia , thongSoKyThuat = :thongSoKyThuat",
+        ExpressionAttributeValues:{
+            ":ten": ten,
+            ":dvt": donViTinh,
+            ":gia": gia,
+            ":thongSoKyThuat": thongSoKyThuat
+        },
+        ReturnValues: "UPDATED_NEW",
+    };
+    dynamoDB.update(paramsUpdateLinhKien,(error,data)=>{
+        if(error)
+            console.log(error);
+        else
+            res.redirect("/");
+    });
+});
+
+/// tim kiem ten linh kien theo ten
+app.post("/timKiemLinhKienTheoTen",(req,res)=>{
+    const ten = req.body.ten;
+    console.log(ten);
+    const paramsTimKiemTheoTen = {
+        TableName : "LinhKien",
+        FilterExpression: "contains(ten, :t)",
+        ExpressionAttributeValues:{
+            ":t" : ten
+        },
+    };
+    dynamoDB.scan(paramsTimKiemTheoTen,(error,data)=>{
+        if(error)
+            console.log(error);
+        else 
+            res.render("index",{
+                linhKien: data.Items
+            });
+    });
+});
+
+// tim kiem tuyet doi
+app.post("/timKiemTuyetDoiTheoTen",(req,res)=>{
+    const ten = req.body.ten;
+    const paramsTimKiemTheoTenTuyetDoi = {
+        TableName : "LinhKien",
+        FilterExpression: "ten = :t",
+        ExpressionAttributeValues:{
+            ":t":ten
+        },
+    };
+    dynamoDB.scan(paramsTimKiemTheoTenTuyetDoi,(error,data)=>{
+        if(error)
+            console.log(error);
+        else 
+            res.render("index",{
+                linhKien : data.Items
+            });
+    });
+})
